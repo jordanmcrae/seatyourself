654a5d275c0e25ffa49a17534a36675e9c1a2d8a
Jordan <jordanmcrae@hotmail.com>
2015-04-23 17:12:08 -0400

    Verified associations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,14 +2,5 @@ class Reservation < ActiveRecord::Base
   belongs_to :restaurant
   belongs_to :user
   validates :party_size, numericality: [only_integer: true]
-  validate :date_is_valid?
 
-
-  private
-
-  def date_is_valid?
-    if !date.is_a?(Date)
-      errors.add(:date, 'must be a valid date')
-    end
-  end
 end
